5babb13b9a6c37d247c31a26d23d705f525917f5
Glib Rulev <velzepooz@gmail.com>
2020-06-16 10:40:52 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-console.log('hello mate academy!');
+const gallery = document.querySelector('#thumbs');
+
+gallery.onclick = function(e) {
+  e.preventDefault();
+
+  const isSmall = e.target.classList.contains('gallery__thumb');
+
+  if (isSmall) {
+    const currentImg = event.target;
+    const largeImage = document.querySelector('#largeImg');
+
+    largeImage.src = currentImg.src.replace('-thumb.jpeg', '.png');
+  }
+};
